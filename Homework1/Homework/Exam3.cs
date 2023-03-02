6fe56f3e89ee06ffe51f355a6a5e5a3cdef12503
DimitarPetrovski <dimitarpetrovski5@gmail.com>
2023-03-02 15:38:44 +0100

    Add existing file

---
+++ Homework1/Homework/Exam3.cs
@@ -0,0 +1,14 @@
+Console.WriteLine("Enter A1: ");
+string a = Console.ReadLine();
+Console.WriteLine("Enter B1: ");
+string b = Console.ReadLine();
+
+int a1;
+int b1;
+
+bool aParse = int.TryParse(a, out b1);
+bool bParse = int.TryParse(b, out a1);
+
+
+Console.WriteLine("A1 is :" + a1);
+Console.WriteLine("B1 is :" + b1);
